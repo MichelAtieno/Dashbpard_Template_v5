6c0c6d4f365d35b9ba3707aadf22b7a13edad023
MichelAtieno <michelatieno23@gmail.com>
2023-05-19 17:37:05 +0300

    Added few JS functions

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '56543',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '56543',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Some Utensils',
+        productNumber: '56544',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Foood',
+        productNumber: '56545',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Handy Microphone',
+        productNumber: '56546',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Mic Stands',
+        productNumber: '56547',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+]
